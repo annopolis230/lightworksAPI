07608b9de911f32ddb88262e56e48166c9fe3d32
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2024-07-30 19:38:07 -0400

    Update app.py
    secrets, unauthorized error handling

--- app.py
+++ app.py
@@ -6,25 +6,34 @@ import mysql.connector
 
 app = Flask(__name__)
 
-key = os.getenv("ENCRYPTION_KEY").encode()
+class UnauthorizedException(Exception):
+        def __init__(self, message, code=None):
+                super().__init__(message)
+                self.code = code
+
+def get_secret(secret):
+        with open(f'{os.getenv(secret)}') as f:
+                return f.read().strip()
+
+key = get_secret("ENCRYPTION_KEY").encode()
 cipher_suite = Fernet(key)
 
 def verify_request(secret, headers):
         received_secret = headers.get('x-api-key')
         if not received_secret:
                 app.logger.warning(f'No API key provided from {headers.get("x-forwarded-for")}') 
-                abort(401)
+                raise UnauthorizedException("No API key",code=1001)
         else:
                 received_secret = received_secret.encode()
         if not hmac.compare_digest(received_secret, secret):
                 app.logger.warning(f'Invalid API key from {headers.get("x-forwarded-for")}')
-                abort(401)
+                raise UnauthorizedException("Invalid API key",code=1002)
 
 def fetch_key(name):
         connection = mysql.connector.connect(
                 host=os.getenv("MARIADB_HOST"),
                 user=os.getenv("MARIADB_USER"),
-                password=os.getenv("MARIADB_PASSWORD"),
+                password=get_secret("MARIADB_PASSWORD"),
                 database=os.getenv("MARIADB_DATABASE")
         )
         cursor = connection.cursor()
@@ -45,6 +54,11 @@ def place():
 @app.errorhandler(404)
 def page_not_found(e):
         return jsonify({'error': 'Unknown route'}), 404
+
+@app.errorhandler(UnauthorizedException)
+def unauthorized(e):
+        response = {'error': 'Resource Unauthorized', 'message': str(e), 'code': e.code}
+        return jsonify(response), 401
 
 if __name__ == '__main__':
         app.run(debug=False, host='192.168.100.30')
